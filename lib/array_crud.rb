ea99e91e37cd66efa19f392b62c841cb168e27d2
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-17 15:28:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.unshift()
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
